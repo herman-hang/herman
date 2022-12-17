577f2b84308775031dad4ec34defffb69b406dc1
xiaohang <wetalk.vip@foxmail.com>
2022-12-17 23:00:44 +0800

    完善框架

--- routers/Router.go
+++ routers/Router.go
@@ -9,9 +9,18 @@ import (
 
 // InitRouter 初始化路由
 // @param *gin.Engine rootEngine 路由引擎
+// @return void
 func InitRouter(rootEngine *gin.Engine) {
+	// 测试路由
+	rootEngine.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"code":    200,
+			"message": "操作成功",
+			"data":    nil,
+		})
+	})
+	// 设置路由前缀
 	api := rootEngine.Group(settings.Config.AppPrefix)
-
 	api.Use(middlewares.Jwt())
 	{
 		// 用户相关路由
