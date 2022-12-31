8ad74a710071f540561ad8bac0cf324c66168bf6
xiaohang <wetalk.vip@foxmail.com>
2022-12-31 22:08:20 +0800

    完善仓储层，JWT；修改枚举功能

--- app/services/user/UserService.go
+++ app/services/user/UserService.go
@@ -20,5 +20,5 @@ func Login(data map[string]interface{}) interface{} {
 	}
 
 	// 返回token
-	return utils.GenerateToken(&utils.UserClaims{Uid: user.Id, Guard: "user"})
+	return utils.GenerateToken(&utils.UserClaims{Uid: user.Id, Guard: "foreground"})
 }
